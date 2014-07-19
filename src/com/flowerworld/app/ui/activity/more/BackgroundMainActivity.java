b90fa50db84f20631cae0d3f136ceca6d21daa6a
chinamima <chinamima@163.com>
2014-07-20 00:23:13 +0800

    新添企业后台界面，主要是fragment

--- src/com/flowerworld/app/ui/activity/more/BackgroundMainActivity.java
+++ src/com/flowerworld/app/ui/activity/more/BackgroundMainActivity.java
@@ -1,17 +1,27 @@
 package com.flowerworld.app.ui.activity.more;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import com.flowerworld.app.R;
-import com.flowerworld.app.ui.base.BaseActivity;
+import com.flowerworld.app.ui.base.BaseFragmentActivity;
+import com.flowerworld.app.ui.fragment.TestFragment;
+import com.flowerworld.app.ui.fragment.background.MemberCenterFragment;
 
 /**
  * Created by guojj4 on 2014/7/16.
  */
-public class BackgroundMainActivity extends BaseActivity implements View.OnClickListener {
+public class BackgroundMainActivity extends BaseFragmentActivity implements View.OnClickListener {
 
+    //widget
     private Button mBtnExit = null;
     private Button mBtnMemberCenter = null;
     private Button mBtnTemplate = null;
@@ -19,6 +29,13 @@ public class BackgroundMainActivity extends BaseActivity implements View.OnClick
     private Button mBtnWechat = null;
 
     private ViewPager mViewPager = null;
+    private FrameLayout mFrameSwitcher = null;
+
+    //selection
+    protected int mSelectionButtonId = View.NO_ID;
+
+    //data
+    private Fragment[] mFragments = new Fragment[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,30 +58,81 @@ public class BackgroundMainActivity extends BaseActivity implements View.OnClick
         mBtnTemplate.setOnClickListener(this);
         mBtnProduct.setOnClickListener(this);
         mBtnWechat.setOnClickListener(this);
+
+        mSelectionButtonId = R.id.background_main_button_member_center;
+        mBtnMemberCenter.setSelected(true);
+
+        initFragments();
+//        mViewPager.setAdapter(new CustomPageAdapter(getSupportFragmentManager()));
+        switchFragment(0);
+    }
+
+    private void initFragments() {
+        mFragments[0] = MemberCenterFragment.newInstance();
+        mFragments[1] = TestFragment.newInstance("模板管理");
+        mFragments[2] = TestFragment.newInstance("产品");
+        mFragments[3] = TestFragment.newInstance("微信平台");
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-        case R.id.background_main_button_exit:
+        int btnId = v.getId();
+        if (btnId == mSelectionButtonId) {
+            return;
+        } else {
+            findViewById(mSelectionButtonId).setSelected(false);
+            findViewById(btnId).setSelected(true);
+            mSelectionButtonId = btnId;
+        }
 
+        int index = 0;
+        switch (btnId) {
+        case R.id.background_main_button_exit:
+            finish();
             break;
 
         case R.id.background_main_button_member_center:
-
+            index = 0;
             break;
 
         case R.id.background_main_button_template:
-
+            index = 1;
             break;
 
         case R.id.background_main_button_product:
-
+            index = 2;
             break;
 
         case R.id.background_main_button_wechat:
-
+            index = 3;
             break;
         }
+        switchFragment(index);
+    }
+
+    private void switchFragment(int index) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.background_main_frame, mFragments[index]);
+        transaction.disallowAddToBackStack();
+        transaction.commit();
+    }
+
+    private class CustomPageAdapter extends FragmentPagerAdapter {
+
+        public CustomPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.length;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return mFragments[i];
+        }
+
+
     }
 }
---
+++ src/com/flowerworld/app/ui/fragment/background/MemberCenterFragment.java
@@ -0,0 +1,75 @@
+package com.flowerworld.app.ui.fragment.background;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.flowerworld.app.R;
+import com.flowerworld.app.ui.base.BaseFragment;
+import com.flowerworld.app.ui.fragment.TestFragment;
+
+/**
+ * Created by guojj4 on 14/07/19.
+ */
+public class MemberCenterFragment extends BaseFragment implements View.OnClickListener {
+
+    private MemberCenterFragment() {
+    }
+
+    private static MemberCenterFragment mSingleInstance = new MemberCenterFragment();
+    public static MemberCenterFragment newInstance() {
+        return mSingleInstance;
+    }
+
+    private Fragment[] mFragments = new Fragment[2];
+
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.member_center_page, container, false);
+    }
+
+    @Override
+    protected void initView() {
+        mFragments[0] = TestFragment.newInstance("欢迎页面");
+        mFragments[1] = TestFragment.newInstance("个人资料");
+
+        findViewById(R.id.member_center_button_welcome).setOnClickListener(this);
+        findViewById(R.id.member_center_button_information).setOnClickListener(this);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void resumeData() {
+
+    }
+
+    private void switchFragment(int index) {
+        FragmentTransaction transaction = getFragmentTransaction();
+        transaction.replace(R.id.member_center_frame, mFragments[index]);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        int index = 0;
+        switch (id) {
+        case R.id.member_center_button_welcome:
+            index = 0;
+            break;
+
+        case R.id.member_center_button_information:
+            index = 1;
+            break;
+        }
+
+        switchFragment(index);
+    }
+}
